61a7f0a468a52af3c03376cdc90ebdb9bf12d01c
Brenda Rayane <brenda.rayane.0018@gmail.com>
2023-09-17 14:20:10 -0300

    initialized application

---
+++ src/main/java/com/teamdrawer/teamdrawerapi/TeamdrawerapiApplication.java
@@ -0,0 +1,13 @@
+package com.teamdrawer.teamdrawerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamdrawerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamdrawerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamdrawer/teamdrawerapi/TeamdrawerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamdrawer.teamdrawerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamdrawerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
